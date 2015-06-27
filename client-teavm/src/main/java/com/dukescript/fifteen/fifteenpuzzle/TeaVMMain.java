15acde5c2a9a085da15cc43d404dbe979f0eea1b
konsoletyper <konsoletyper@gmail.com>
2015-06-27 13:03:23 +0300

    Add TeaVM client

---
+++ client-teavm/src/main/java/com/dukescript/fifteen/fifteenpuzzle/TeaVMMain.java
@@ -0,0 +1,11 @@
+package com.dukescript.fifteen.fifteenpuzzle;
+
+/**
+ *
+ * @author Alexey Andreev
+ */
+public class TeaVMMain {
+    public static void main(String[] args) {
+        Main.onPageLoad();
+    }
+}
